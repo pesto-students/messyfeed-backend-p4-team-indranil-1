869749e10d3828b35ddb5fa1f96be9355b208ff6
Pradip Bedre <bedrepradip0@gmail.com>
2022-12-22 21:50:17 +0530

    all auth done

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,12 @@
 import express from "express";
-import { verifyToken } from "../verifyToken.js";
 const router = express.Router();
 import { signup, signin } from "../controllers/auth.js";
 
 // POST /auth/signup
-router.post("/signup", verifyToken, signup);
+router.post("/signup", signup);
 
 // POST /auth/login
-router.post("/signin", verifyToken, signin);
+router.post("/signin", signin);
 
 // POST /googlelogin
 
