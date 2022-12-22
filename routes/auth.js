e65607e0c8e567e51ac2cf313856d55163dceafa
Pradip Bedre <bedrepradip0@gmail.com>
2022-12-22 21:42:43 +0530

    varifytoken file added

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,15 @@
 import express from "express";
+import { verifyToken } from "../verifyToken.js";
 const router = express.Router();
 import { signup, signin } from "../controllers/auth.js";
 
 // POST /auth/signup
-router.post("/signup", signup);
+router.post("/signup", verifyToken, signup);
 
 // POST /auth/login
-router.post("/signin", signin);
+router.post("/signin", verifyToken, signin);
 
 // POST /googlelogin
-
-
 
 //export default router;
 export default router;
